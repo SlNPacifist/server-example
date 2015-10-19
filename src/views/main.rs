6703e4f76b411cfd9608e52bc13a106f8d5deeef
slnpacifist <slnpacifist@gmail.com>
2015-10-19 21:51:22 +0800

    Убрал список потребителей с главной

--- src/views/main.rs
+++ src/views/main.rs
@@ -5,19 +5,12 @@ use iron::headers::*;
 use persistent::Read;
 use iron_mountrouter::{Router, MethodPicker};
 use dtl::{Context, HashMapContext};
-use models::Consumer;
-use dtl_impls::ConsumerList;
-use db::Database;
 use views::TemplateCompilerKey;
 
 
 fn entry(req: &mut Request) -> IronResult<Response> {
-	let pool = req.get::<Read<Database>>().unwrap();
-	let consumers = Consumer::all(&pool.get().unwrap());
 	let template_compiler = req.get::<Read<TemplateCompilerKey>>().unwrap();
-    let mut ctx = HashMapContext::new();
-    let tmp: ConsumerList = consumers.into();
-    ctx.set("consumers", Box::new(tmp));
+    let ctx = HashMapContext::new();
     let response_text = template_compiler.render(Path::new("main.htmt"), &ctx).unwrap();
     let mut res = Response::with((status::Ok, response_text));
     res.headers.set(ContentType::html());
