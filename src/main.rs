7c7094ee3a13419ca0a47c03353310d91148087b
slnpacifist <slnpacifist@gmail.com>
2015-10-22 02:00:54 +0800

    Добавил команды командной строки для старта сервера и для добавления нового админа

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ extern crate chrono;
 extern crate urlencoded;
 extern crate toml;
 extern crate rustc_serialize;
+extern crate clap;
 
 mod models;
 mod views;
@@ -24,13 +25,44 @@ mod session;
 mod config;
 
 use std::path::Path;
+use clap::{App, SubCommand};
+use db::DbConnectionPool;
+use models::{User, UserRole};
+use config::Config;
 
 
 fn main() {
 	let config = config::get_config(Path::new("./config.toml"));
 	let pool = db::get_db_connection_pool(&config.db_connection_string);
+
+    let matches = App::new("Zima water supplies web server")
+        .subcommand(SubCommand::with_name("start")
+            .about("starts web server"))
+        .subcommand(SubCommand::with_name("add-admin")
+        	.about("adds new admin")
+        	.args_from_usage("<name> 'Username for created admin'
+        					  <password> 'Password for created admin'"))
+        .subcommand_required(true)
+        .get_matches();
+    if matches.subcommand_matches("start").is_some() {
+    	start_server(config, pool);
+	} else if let Some(ref sub_matches) = matches.subcommand_matches("add-admin") {
+    	let name = sub_matches.value_of("name").unwrap();
+    	let password = sub_matches.value_of("password").unwrap();
+    	add_admin(pool, name, password);
+    }
+}
+
+fn start_server(config: Config, pool: DbConnectionPool) {
 	let root = views::get_root(pool);
 	println!("Server started");
     iron::Iron::new(root).http(&config.http_address as &str).expect(
     	&format!("Could not start server at {}", config.http_address));
+}
+
+fn add_admin(pool: DbConnectionPool, name: &str, password: &str) {
+	let connection = pool.get().unwrap();
+	User::create(&connection, name.to_string(), password.to_string(), UserRole::Admin, None)
+		.expect("Could not add admin");
+	println!("Admin {} added", name);
 }
