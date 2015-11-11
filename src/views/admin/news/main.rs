cb0113072633569297b5bd76a755033a56146940
slnpacifist <slnpacifist@gmail.com>
2015-11-11 20:13:33 +0800

    Убрал лишние скобки

--- src/views/admin/news/main.rs
+++ src/views/admin/news/main.rs
@@ -7,12 +7,10 @@ use views::utils::*;
 
 
 pub fn all_news(req: &mut Request) -> IronResult<Response> {
-	{
-		let connection = req.get::<Read<Database>>()
-			.expect("Could not get connection pool in admin::news::all_news")
-			.get().expect("Could not get connection in admin::news::all_news");
-		let news = News::ordered_by_date(&connection, 20);
-		update_var(req, "news", NewsList::new(news)); 
-	}
+	let connection = req.get::<Read<Database>>()
+		.expect("Could not get connection pool in admin::news::all_news")
+		.get().expect("Could not get connection in admin::news::all_news");
+	let news = News::ordered_by_date(&connection, 20);
+	update_var(req, "news", NewsList::new(news)); 
 	render_ok(req, "admin/news/list.htmt")
 }
